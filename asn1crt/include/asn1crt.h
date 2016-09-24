d4e073412fb5504978c6ce09642326c1c49df867
Vlad Gheorghe <vlad.gheorghe@gmail.com>
2016-09-25 02:58:51 +0300

    reorder includes

--- asn1crt/include/asn1crt.h
+++ asn1crt/include/asn1crt.h
@@ -1,10 +1,10 @@
 #ifndef ASN1SCC_ASN1CRT_H_
 #define ASN1SCC_ASN1CRT_H_
 
-#include "asn1scc/acn.h"
 #include "asn1scc/core.h"
 #include "asn1scc/BitStream.h"
 #include "asn1scc/ByteStream.h"
+#include "asn1scc/acn.h"
 #include "asn1scc/ber.h"
 #include "asn1scc/xer.h"
 
